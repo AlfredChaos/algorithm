996ad4fa956a17a842c35b366bc326c47d4a1671
AlfredChaos <Caojie_the42@outlook.com>
2024-05-23 22:35:15 +0800

    add detect cycle

---
+++ List/detectCycle.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil {
+		slow = slow.Next
+		if fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next
+		if fast == slow {
+			index := head
+			for index != slow {
+				index = index.Next
+				slow = slow.Next
+			}
+			return index
+		}
+	}
+	return nil
+}
